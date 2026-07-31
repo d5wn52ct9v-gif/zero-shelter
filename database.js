ad35412b38d8da0f213f366c47c124f16375112e
d5wn52ct9v-gif <d5wn52ct9v@privaterelay.appleid.com>
2026-08-01 00:53:48 +0800

    Create database.js

---
+++ database.js
@@ -0,0 +1,27 @@
+const residents = {
+
+  "ZS-A-91-0427": {
+    name: "沈語晴",
+    password: "0427",
+    level: "二級居民",
+    area: "A區",
+    credit: 96,
+    status: "正常",
+    loyalty: 91,
+    medical: "健康",
+    page: "home.html"
+  },
+
+  "ZS-B-90-0101": {
+    name: "林邵庭",
+    password: "0101",
+    level: "三級居民",
+    area: "B區",
+    credit: 82,
+    status: "觀察中",
+    loyalty: 67,
+    medical: "正常",
+    page: "home.html"
+  }
+
+};
