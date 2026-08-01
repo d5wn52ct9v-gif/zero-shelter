d46980e64eaf23bb3c41176e9f6ad3767d5e24eb
d5wn52ct9v-gif <d5wn52ct9v@privaterelay.appleid.com>
2026-08-01 10:41:45 +0800

    Update database.js

--- database.js
+++ database.js
@@ -1,27 +1,23 @@
 const residents = {
 
   "ZS-A-91-0427": {
-    name: "沈語晴",
     password: "0427",
+    name: "沈語晴",
+    id: "ZS-A-91-0427",
     level: "二級居民",
     area: "A區",
     credit: 96,
-    status: "正常",
-    loyalty: 91,
-    medical: "健康",
-    page: "home.html"
+    status: "正常"
   },
 
   "ZS-B-90-0101": {
-    name: "林邵庭",
     password: "0101",
+    name: "邵庭",
+    id: "ZS-B-90-0101",
     level: "三級居民",
     area: "B區",
     credit: 82,
-    status: "觀察中",
-    loyalty: 67,
-    medical: "正常",
-    page: "home.html"
+    status: "觀察中"
   }
 
 };
